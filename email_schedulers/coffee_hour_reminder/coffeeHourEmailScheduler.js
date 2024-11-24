ad96f79dd06d8e085db476b55240a95035a4a23f
andrew-markopoulos <andrew.markopoulos@softrams.com>
2024-11-24 00:25:13 -0500

    notify the secretary if missing email

--- email_schedulers/coffee_hour_reminder/coffeeHourEmailScheduler.js
+++ email_schedulers/coffee_hour_reminder/coffeeHourEmailScheduler.js
@@ -6,7 +6,9 @@ var ATTACHMENTS = [DriveApp.getFileById('1shafaAPgLAMYFEvoqQUsI9pcCmpT7F8E')] //
 function sendReminderEmails() {
   // Open the specific spreadsheet using its ID
   var spreadsheet = SpreadsheetApp.openById(SPREADSHEET_ID);
-  var sheet = spreadsheet.getActiveSheet();
+  var sheet = spreadsheet.getSheetByName('Schedule');
+  var spreadsheetName = spreadsheet.getName();
+  var spreadsheetUrl = spreadsheet.getUrl();
   
   // Get data starting from A3
   var dataRange = sheet.getRange("A2:D" + sheet.getLastRow());
@@ -22,29 +24,47 @@ function sendReminderEmails() {
     var family = row[2]
     var emails = row[3];
     
+    // Calculate the date six days before the event
+    var secretaryNotifDate = new Date(eventDate.getTime());
+    secretaryNotifDate.setDate(secretaryNotifDate.getDate() - 6);
+
     // Calculate the date four days before the event
-    var reminderDate = new Date(eventDate.getTime());
-    reminderDate.setDate(reminderDate.getDate() - 4);
+    var hostReminderDate = new Date(eventDate.getTime());
+    hostReminderDate.setDate(hostReminderDate.getDate() - 4);
+
+    // Check if today is the secretary notification date
+    if (today.toDateString() === secretaryNotifDate.toDateString()) {
+      // Send the email
+      sendNotificationEmail(family, eventDate, hostReminderDate, spreadsheetName, spreadsheetUrl);
+    }
     
-    // Check if today is the reminder date
-    if (today.toDateString() === reminderDate.toDateString()) {
+    // Check if today is the host reminder date
+    if (today.toDateString() === hostReminderDate.toDateString()) {
       // Send the email
       sendEmail(family, eventDate, emails);
     }
   }
 }
 
-// function to get the instructions for coffee hour
-function getCoffeeHourInstructions() {
-  try {
-    var doc = DocumentApp.openById(EMAIL_TEMPLATE_ID);
-    var body = doc.getBody();
-    return body.getText();
-  } catch (e) {
-    Logger.log('Error retrieving document: ' + e.message);
-    return '';
-  }
+// function to send the notification email to the secretary
+function sendNotificationEmail(family, eventDate, hostReminderDate, spreadsheetName, spreadsheetUrl) {
+  var userEmail = Session.getActiveUser().getEmail();
+  var subject = "Missing Coffee Hour Host Emails " + eventDate.toDateString();
+  var message = `Dear Secretary,<br>
+  The following family/group is scheduled to host coffee hour on <b>${eventDate.toDateString()}</b> but we are missing their email addresses:<br>
+  <b>${family}</b><br>
+  Please update the <b>${spreadsheetName}</b> Google Sheet with the correct email addresses before the reminder email sends at 5:00AM on <b>${hostReminderDate.toDateString()}</b>.
+  You can access the spreadsheet <a href="${spreadsheetUrl}">here</a>.<br><br>
+  Thank you!
+  This is an automated message. Please do not reply.`;
+
+  MailApp.sendEmail({
+    to: userEmail,
+    subject: subject,
+    htmlBody: message
+  });
 }
+
 
 function sendEmail(family, eventDate, emails) {
   var emailAddresses = formatEmailAddresses(emails); 
@@ -69,4 +89,16 @@ function formatEmailAddresses(emailString) {
     throw new Error('Input must be a string');
   }
   return emailString.split(',').map(email => email.trim()).join(', ');
+}
+
+// function to get the instructions for coffee hour
+function getCoffeeHourInstructions() {
+  try {
+    var doc = DocumentApp.openById(EMAIL_TEMPLATE_ID);
+    var body = doc.getBody();
+    return body.getText();
+  } catch (e) {
+    Logger.log('Error retrieving document: ' + e.message);
+    return '';
+  }
 }
