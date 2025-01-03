52a3dcf4f07256a2ea3ec5cb7220a93e2b413115
andrew-markopoulos <andrew.markopoulos@softrams.com>
2025-01-02 21:42:01 -0500

    adding form id

--- google_app_scripts/out_of_office_form/outOfTheOfficeFormNotification.js
+++ google_app_scripts/out_of_office_form/outOfTheOfficeFormNotification.js
@@ -67,7 +67,7 @@ function doGet(e) {
 
 // Set up the trigger to run the onFormSubmit function when a form is submitted
 function createTrigger() {
-  var form = FormApp.openById('YOUR_FORM_ID'); // Replace with your form ID
+  var form = FormApp.openById('119iFJQUDm1GsKjrqUNvjdSJdGFzZf_qIaCapY0EZeFQ'); // Replace with your form ID
   ScriptApp.newTrigger('onFormSubmit')
            .forForm(form)
            .onFormSubmit()
